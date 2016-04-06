9bcf0929b4d3b426bdcfaab43c7a280aeb44ce7b
Alexis BRENON <brenon.alexis@gmail.com>
2016-04-06 14:26:21 +0200

    Update version, changelog, etc.

--- include/boblight/install_boblight.sh
+++ include/boblight/install_boblight.sh
@@ -4,8 +4,11 @@ set -eux
 
 # Install boblight from repo
 svn checkout http://boblight.googlecode.com/svn/trunk/ boblight
-cd boblight && ./configure --prefix=/usr --without-portaudio && make && sudo make install
-cd -
+cd boblight && \
+  ./configure --prefix=/usr --without-portaudio --without-libusb --without-spi --without-x11 --without-opengl && \
+  make && \
+  sudo make install && \
+  cd -
 
 # Test boblight availability
 which boblightd || exit 1
