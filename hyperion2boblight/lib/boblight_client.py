399619d81af9d0db22e40d90fedae6bf8d42f6e9
Alexis BRENON <brenon.alexis@gmail.com>
2016-04-07 08:29:09 +0200

    BoblightClient small refactoring

--- hyperion2boblight/lib/boblight_client.py
+++ hyperion2boblight/lib/boblight_client.py
@@ -86,68 +86,25 @@ class BoblightClient:
     Client which connect to a Boblight server and send it commands from a PriorityList
     """
     def __init__(self, server_address, priority_list):
-        self.server_address = server_address
-        self.priority_list = priority_list
-        self.effect_stop_event = threading.Event()
-        self.effect_threads = []
-        self.command = None
-        self.lights = {}
+        self.logger = logging.getLogger("BoblightClient")
+        self.priority_list = priority_list # Priority list from which get the command
+
+        self.lights = {} # Boblight server lights
+        self.message = "" # Message to send to the server
+
+        self.effect_threads = [] # List of launched effect
+        self.effect_stop_event = threading.Event() # Threading event to stop effects
+
         try:
-            self.socket = socket.create_connection(self.server_address)
+            self.socket = socket.create_connection(server_address)
         except socket.error as socket_error:
             logging.error(
                 "Unable to create the connection to boblight server '%s:%d'",
-                self.server_address[0],
-                self.server_address[1])
+                server_address[0],
+                server_address[1])
             raise socket_error
-        logging.info('Boblight connection accepted.')
+        self.logger.info('Boblight connection accepted.')
 
-    def say_hello(self):
-        """ Initiate communication with a hand shaking """
-        self.send_message('hello\n')
-        data = self.socket.recv(4096)
-        if data != bytes('hello\n', "utf8"):
-            logging.critical("Incorrect response from boblight server: '%s'", str(data, 'utf-8'))
-        return data
-
-    def get_lights(self):
-        """ Get lights connected to the Boblight server """
-        self.send_message('get lights\n')
-        data = str(self.socket.recv(4096), "utf-8").strip()
-        lines = data.split('\n')
-        if lines[0].split()[0] != "lights":
-            logging.error("Unable to enumerate lights")
-        else:
-            for line in lines[1:]:
-                (_, name, _, vscan_0, vscan_1, hscan_0, hscan_1) = line.split(' ')
-                self.lights[name] = BoblightLight(
-                    name,
-                    (hscan_0, hscan_1),
-                    (vscan_0, vscan_1)
-                )
-            assert int(lines[0].split()[1]) == len(self.lights)
-        logging.debug("Found %s lights: %s", len(self.lights), self.lights)
-        return data
-
-    def set_lights(self, red, green=None, blue=None):
-        """ Return the string to turn all light to the asked colors """
-        if isinstance(red, (list, tuple)):
-            blue = red[2]
-            green = red[1]
-            red = red[0]
-
-        message = ""
-        for light in self.lights.values():
-            message = message + 'set light %s rgb %f %f %f\n' % (
-                light.name,
-                red/255.0,
-                green/255.0,
-                blue/255.0)
-        return message
-
-    def set_priority(self, priority):
-        """ Return the string to send to set the priority """
-        return 'set priority %d\n' % (priority)
 
     def run(self):
         """
@@ -158,66 +115,124 @@ class BoblightClient:
         self.get_lights()
 
         try:
-            self.command = self.priority_list.get_first()
-            self.handle_command()
+            command = self.priority_list.get_first()
+            self.handle_command(command)
         except Empty:
             pass
+
+        # wait for new command
         while True:
-            # wait for new command
             try:
-                self.command = self.priority_list.wait_new_item()
-                if self.command[1] == 'quit':
-                    self.effect_stop_event.set()
+                command = self.priority_list.wait_new_item()
+                if command[1] == 'quit':
                     break
             except Empty:
-                self.command = None
-            self.handle_command()
+                command = None
+            self.handle_command(command)
 
-        logging.info('Shutting Down')
+        self.logger.info('Shutting Down')
         self.effect_stop_event.set()
         for thread in self.effect_threads:
             thread.join()
         self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
 
-    def handle_command(self):
+    def handle_command(self, command):
         """ Main worker """
-        message = ""
         # No command given, turn off lights
-        if self.command is None:
-            logging.debug(
-                "Turning off the lights"
-            )
-            message += self.set_priority(0)
-            message += self.set_lights(0, 0, 0)
+        if command is None:
+            self.logger.debug("Turning off the lights")
+            self.set_priority(0)
+            self.set_all_lights((0, 0, 0))
         else:
-            logging.debug(
+            self.logger.debug(
                 'Executing %s:%s',
-                self.command[0],
-                self.command[1]
+                command[0],
+                command[1]
             )
             # Stop any running effect
             self.effect_stop_event.set()
             # Handle classic 'color' command
-            if isinstance(self.command[1], list):
-                message += self.set_priority(self.command[0])
-                message += self.set_lights(self.command[1])
+            if isinstance(command[1], list):
+                self.set_priority(command[0])
+                self.set_all_lights(
+                    tuple([float(color)/255 for color in command[1]])
+                )
             # Handle rainbow effect
-            elif isinstance(self.command[1], str) and self.command[1] == 'Rainbow':
-                message += self.set_priority(self.command[0])
+            elif isinstance(command[1], str) and command[1] == 'Rainbow':
+                self.set_priority(command[0])
                 self.effect_threads.append(self.EffectThread(self, rainbow.RainbowEffect()))
                 self.effect_threads[-1].start()
             else:
-                logging.warning(
+                self.logger.warning(
                     "Command not recognized : %s",
-                    self.command[0]
+                    command[0]
                 )
         # Actually send commands to the Boblight server
-        if message != "":
-            self.send_message(message)
+        self.send()
 
-    def send_message(self, message):
-        self.socket.sendall(bytes(message, "utf8"))
+    def say_hello(self):
+        """ Initiate communication with a hand shaking """
+        self.message = 'hello\n'
+        self.send()
+
+        data = self.socket.recv(4096)
+        if data != bytes('hello\n', "utf8"):
+            self.logger.critical(
+                ":say_hello(): Incorrect response from boblight server: '%s'",
+                str(data, 'utf-8'))
+
+    def get_lights(self):
+        """ Get lights connected to the Boblight server """
+        self.message = 'get lights\n'
+        self.send()
+
+        data = str(self.socket.recv(4096), "utf-8").strip()
+        lines = data.split('\n')
+        if lines[0].split()[0] != "lights":
+            self.logger.error("Unable to enumerate lights")
+        else:
+            for line in lines[1:]:
+                (_, name, _, vscan_0, vscan_1, hscan_0, hscan_1) = line.split(' ')
+                self.lights[name] = BoblightLight(
+                    name,
+                    (hscan_0, hscan_1),
+                    (vscan_0, vscan_1)
+                )
+            if int(lines[0].split()[1]) != len(self.lights):
+                self.logger.warning(
+                    "%s light(s) announced and %s found.",
+                    lines[0].split()[1], len(self.lights)
+                )
+        self.logger.debug("Found %s lights: %s", len(self.lights), self.lights)
+        return data
+
+    def set_light(self, light, color):
+        """ Set the message to set the light to the passed color.
+        color must be an (r,g,b) tuple with values between 0 and 1. """
+        self.message += 'set light {0} rgb {1[0]:.6f} {1[1]:.6f} {1[2]:.6f}\n'.format(
+            light.name,
+            color
+        )
+
+
+    def set_all_lights(self, color):
+        """ Set the message to turn all lights to the asked color.
+        color must be an (r,g,b) tuple with values between 0.0 and 1.0. """
+        for light in self.lights.values():
+            self.set_light(light, color)
+
+    def set_priority(self, priority):
+        """ Set the message to define the priority of the client """
+        self.message += 'set priority {}\n'.format(priority)
+
+    def send(self):
+        """
+        Send the current message to the the server and reset it.
+        """
+        if self.message != "":
+            self.socket.sendall(bytes(self.message, "utf8"))
+            self.message = ""
 
 
     # Define a thread to handle effects
@@ -235,11 +250,10 @@ class BoblightClient:
         def run(self):
             self.client.effect_stop_event.clear()
             while not self.client.effect_stop_event.wait(self.speed):
-                message = ""
                 for light in self.client.lights.values():
                     color = self.effect.get_color(light)
-                    message += "set light {} rgb {} {} {}\n".format(light.name, *color)
-                self.client.send_message(message)
+                    self.client.set_light(light, color)
+                self.client.send()
                 self.effect.increment()
 
 
